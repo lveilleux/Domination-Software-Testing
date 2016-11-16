f0425a547dd2980e91fbca1f736cba9fee4d7151
Luke <lveilleux13@gmail.com>
2016-11-16 15:51:57 -0500

    Updated reports and AITest
    updated uncommenting AISimTest, updated report.xml's

--- test/junit/net/yura/domination/engine/ai/AISimulationTest.java
+++ test/junit/net/yura/domination/engine/ai/AISimulationTest.java
@@ -21,81 +21,81 @@ public class AISimulationTest extends TestCase{
         
 	@Test
 	public void testSomething() throws InterruptedException {
-            /*
+            
             //Still running 6 hours in...
             //TODO: Fix this test so it actually completes
-		RiskUIUtil.parseArgs(new String[] {});
-		final Risk risk = new Risk();
-		risk.addRiskListener(new RiskAdapter() {
-
-		    public void sendMessage(String output, boolean redrawNeeded, boolean repaintNeeded) {
-		    	if (debug) {
-		    		System.out.print(output+"\n");
-		    	}
-		    }
-
-		    public void needInput(int s) {
-		    	synchronized (risk) {
-		    		risk.notifyAll();
-				}
-		    }
-
-		    public void noInput() {
-
-		    }
-
-		} );
-		AIManager.setWait(0);
-		long start = System.currentTimeMillis();
-		for (int i = 0; i < 300; i++) {
-			playGame(risk);
-		}
-		System.out.println(easy + " " + avg + " " + hard + " " + other);
-		if (debug) {
-			System.out.println(System.currentTimeMillis()-start);
-		}
-		assertTrue(easy <= avg);
-		assertTrue(avg <= hard);
-		risk.kill();
-		risk.join();
-	}
-
-	private void playGame(Risk risk) throws InterruptedException {
-		risk.parser("closegame");
-		synchronized (risk) {
-			while (risk.getGame() != null) {
-				risk.wait();
-			}
-		}
-		risk.parser("newgame");
-		
-		risk.parser("newplayer ai easy 6 6");
-		risk.parser("newplayer ai average 2 2");
-		risk.parser("newplayer ai hard 1 1");
-		risk.parser("newplayer ai easy 3 3");
-		risk.parser("newplayer ai average 4 4");
-		risk.parser("newplayer ai hard 5 5");
-		
-		risk.parser("startgame domination fixed recycle");
-		
-		synchronized (risk) {
-			while (risk.getGame() == null || risk.getGame().getState() != RiskGame.STATE_GAME_OVER) {
-				risk.wait();
-			}
-			Player p = risk.getGame().getCurrentPlayer();
-			if (p.getType() == AIDomination.PLAYER_AI_AVERAGE) {
-				avg++;
-			} else if (p.getType() == AIDomination.PLAYER_AI_EASY) {
-				easy++;
-			} else if (p.getType() == AIDomination.PLAYER_AI_HARD) {
-				hard++;
-			} else {
-				other++;
-			}
-			if (debug) {
-				System.out.println(p);
-			}
-		}
-        */
+//		RiskUIUtil.parseArgs(new String[] {});
+//		final Risk risk = new Risk();
+//		risk.addRiskListener(new RiskAdapter() {
+//
+//		    public void sendMessage(String output, boolean redrawNeeded, boolean repaintNeeded) {
+//		    	if (debug) {
+//		    		System.out.print(output+"\n");
+//		    	}
+//		    }
+//
+//		    public void needInput(int s) {
+//		    	synchronized (risk) {
+//		    		risk.notifyAll();
+//				}
+//		    }
+//
+//		    public void noInput() {
+//
+//		    }
+//
+//		} );
+//		AIManager.setWait(0);
+//		long start = System.currentTimeMillis();
+//		for (int i = 0; i < 300; i++) {
+//			playGame(risk);
+//		}
+//		System.out.println(easy + " " + avg + " " + hard + " " + other);
+//		if (debug) {
+//			System.out.println(System.currentTimeMillis()-start);
+//		}
+//		assertTrue(easy <= avg);
+//		assertTrue(avg <= hard);
+//		risk.kill();
+//		risk.join();
+//	}
+//
+//	private void playGame(Risk risk) throws InterruptedException {
+//		risk.parser("closegame");
+//		synchronized (risk) {
+//			while (risk.getGame() != null) {
+//				risk.wait();
+//			}
+//		}
+//		risk.parser("newgame");
+//		
+//		risk.parser("newplayer ai easy 6 6");
+//		risk.parser("newplayer ai average 2 2");
+//		risk.parser("newplayer ai hard 1 1");
+//		risk.parser("newplayer ai easy 3 3");
+//		risk.parser("newplayer ai average 4 4");
+//		risk.parser("newplayer ai hard 5 5");
+//		
+//		risk.parser("startgame domination fixed recycle");
+//		
+//		synchronized (risk) {
+//			while (risk.getGame() == null || risk.getGame().getState() != RiskGame.STATE_GAME_OVER) {
+//				risk.wait();
+//			}
+//			Player p = risk.getGame().getCurrentPlayer();
+//			if (p.getType() == AIDomination.PLAYER_AI_AVERAGE) {
+//				avg++;
+//			} else if (p.getType() == AIDomination.PLAYER_AI_EASY) {
+//				easy++;
+//			} else if (p.getType() == AIDomination.PLAYER_AI_HARD) {
+//				hard++;
+//			} else {
+//				other++;
+//			}
+//			if (debug) {
+//				System.out.println(p);
+//			}
+//		}
+//        
         }
 }
